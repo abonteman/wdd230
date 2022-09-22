50522c57c4a1b7217260978bfa68cade6fad8f15
abonteman <113268526+abonteman@users.noreply.github.com>
2022-09-22 18:24:07 +1400

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
-/*
-alert(document.lastModified);
-// returns: Tuesday, December 16, 2017 11:09:42
-
-if (Date.parse(document.lastModified) > parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*=\s*([^;]*).*$)|^.*$/, "$1") || "0")) {
-    document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname}`;
-    alert("This page has changed!");
-  }
-
-
- const lastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*=\s*([^;]*).*$)|^.*$/, "$1"));
-const lastModif = Date.parse(document.lastModified);
-
-if (isNaN(lastVisit) || lastModif > lastVisit) {
-  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname}`;
-
-  if (isFinite(lastVisit)) {
-    alert("This page has been changed!");
-  }
-}
-*/
-let oLastModif = new Date(document.lastModified);
-let nLastModif = Date.parse(document.lastModified);
-
 let text = document.lastModified;
 document.getElementById("date").innerHTML = text;
