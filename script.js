3779683edb025a41e8bdd81b3ab0733009f57251
abonteman <113268526+abonteman@users.noreply.github.com>
2022-09-22 18:20:50 +1400

    script js update

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
+/*
 alert(document.lastModified);
 // returns: Tuesday, December 16, 2017 11:09:42
 
-let oLastModif = new Date(document.lastModified);
-let nLastModif = Date.parse(document.lastModified);
-
-/*
 if (Date.parse(document.lastModified) > parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*=\s*([^;]*).*$)|^.*$/, "$1") || "0")) {
     document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname}`;
     alert("This page has changed!");
   }
-*/
+
 
  const lastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*=\s*([^;]*).*$)|^.*$/, "$1"));
 const lastModif = Date.parse(document.lastModified);
@@ -21,6 +18,9 @@ if (isNaN(lastVisit) || lastModif > lastVisit) {
     alert("This page has been changed!");
   }
 }
+*/
+let oLastModif = new Date(document.lastModified);
+let nLastModif = Date.parse(document.lastModified);
 
 let text = document.lastModified;
 document.getElementById("date").innerHTML = text;
